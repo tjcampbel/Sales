687a1634f98e364133054a40abdc7c4a3a6e327d
tjcampbel <47730819+tjcampbel@users.noreply.github.com>
2020-04-16 12:35:06 -0400

    Tests added

--- src/src/Test/TestRectangle.java
+++ src/src/Test/TestRectangle.java
@@ -1,5 +1,22 @@
 package Test;
 
+
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import TDDD.src.src
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class TestRectangle {
+
+  @Test
+  public void testPerimeter(){
+ Rectangle rectangle = new Rectangle();
+
+ Assertions.assertTrue(8 == rectangle.perimeter());
+
+  }
+
+
 
 }
