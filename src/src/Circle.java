9be6855be6c8af5abc8dbffd19d214af4a4a4c40
rfcarnemolla <rfcarnemolla@wpi.edu>
2020-04-16 12:34:03 -0400

    created rectangle

--- src/src/Circle.java
+++ src/src/Circle.java
@@ -1,3 +1,12 @@
-public class Circle {
+public class Circle implements IShape {
 
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
 }
--- src/src/Rectangle.java
+++ src/src/Rectangle.java
@@ -1,3 +1,20 @@
-public class Rectangle {
+public class Rectangle implements IShape{
+    //private
+    double length;
+    double width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getPerimeter(){
+        return (2 * this.length) + (2 * this.width);
+    }
+
+    @Override
+    public double getArea() {
+        return this.length * this.width;
+    }
 }
